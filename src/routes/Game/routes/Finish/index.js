098289fd9f0fed2f7493c88e180a3deadd37d326
Yuriy Faizov <yuriy.faizov@gmail.com>
2021-02-14 22:22:22 +0300

    Добавление карты к себе.

--- src/routes/Game/routes/Finish/index.js
+++ src/routes/Game/routes/Finish/index.js
@@ -1,16 +1,20 @@
 import { useState, useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom'
 import cn from 'classnames'
+import database from '../../../../service/firebase';
+
 import PokemonCard from '../../../../components/PokemonCard'
 
+import { FireBaseContext } from '../../../../context/firebaseContext';
 import { PokemonContext } from '../../../../context/pokemonContext';
 
 import s from './style.module.css';
 
 const FinishPage = () => {
+    const firebase = useContext(FireBaseContext);
     const pokemonContext = useContext(PokemonContext);
 
-    console.log(pokemonContext.pokemons2)
+
     const history = useHistory();
     const handleStartGame = () => {
         history.push(`/game`)
@@ -22,6 +26,16 @@ const FinishPage = () => {
     if (Object.keys(pokemonContext.pokemons).length === 0) {
         history.replace('/game');
     }
+
+    const handleClickCard = (key) => {
+        const pokemon = {...pokemonContext.pokemons2[key]};
+        console.log(pokemon)
+        firebase.addPokemon(pokemon)
+ 
+        history.push(`/game`)
+
+          
+      }
     return (
         <div className={s.flex}>
              <div className={s.playerOne}>
@@ -54,6 +68,11 @@ const FinishPage = () => {
                             isActive={true}
                             isSelected={selected}
                             className={s.card}
+                            handleClickCard={() => {
+                                if (Object.keys(pokemonContext.pokemons2) || selected) {
+                                  handleClickCard(key)
+                                }
+                               }}
                         />
                     )}
             </div>
